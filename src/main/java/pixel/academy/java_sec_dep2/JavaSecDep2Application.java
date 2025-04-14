50186d9f9a83f7716c793394d0f058b1c023fd98
Sorin Gore <soringoredev@gmail.com>
2025-04-14 19:29:46 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/java_sec_dep2/JavaSecDep2Application.java
@@ -0,0 +1,13 @@
+package pixel.academy.java_sec_dep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSecDep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSecDep2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/java_sec_dep2/JavaSecDep2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.java_sec_dep2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSecDep2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
